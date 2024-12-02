cd913b21a3c98f2cdc5d46d9fa9e0aca209d9b78
flupe <lucas@escot.me>
2024-12-02 10:04:03 +0100

    first commit

---
+++ 02.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE BlockArguments #-}
+import Data.List.Split
+import Data.Functor ((<&>))
+
+type Report = [Int]
+
+safe1 :: Report -> Bool
+safe1 r =
+  let diff  = zipWith (-) (tail r) r
+      range = flip all (map abs diff) \v -> v > 0 && v < 4
+      incr  = all (> 0) diff
+      decr  = all (< 0) diff
+   in range && (incr || decr)
+
+subs :: Report -> [Report]
+subs [] = []
+subs (x:xs) = xs : ((x :) <$> subs xs)
+
+safe2 :: Report -> Bool
+safe2 r = any safe1 (r : subs r)
+
+main :: IO ()
+main = do
+  reports :: [Report]
+    <- readFile "inputs/2"
+       <&> lines
+       <&> map (map read . splitOn " ")
+
+  print $ length $ filter safe1 reports
+  print $ length $ filter safe2 reports
