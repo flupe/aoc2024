734ff71f0067741f74ac135a272ff8dcb4ef378f
flupe <lucas@escot.me>
2024-12-18 21:28:05 +0100

    simpler bfs for part 1

--- src/Day18.hs
+++ src/Day18.hs
@@ -1,89 +1,132 @@
 {-# LANGUAGE NoImplicitPrelude, OverloadedStrings, BlockArguments #-}
-{-# LANGUAGE PatternSynonyms, ViewPatterns #-}
+{-# LANGUAGE PatternSynonyms, ViewPatterns, DeriveGeneric, DeriveAnyClass #-}
 
 module Day18 (main) where
 
 import AOC
 
-import Data.Ord (Down(Down))
+import Control.Applicative.Combinators (sepBy)
+import Data.Ord (Down(Down, getDown))
 import Data.Array (Array, (!))
 import Data.Array.MArray
+import Data.Array.Base (unsafeWrite, unsafeRead, unsafeAt)
 import Data.Array.IO (IOArray)
-import Data.Array.ST (STArray)
+import Data.Array.ST (STArray, STUArray)
 import Data.Foldable (foldrM)
+import Data.List ((!?))
+import Data.Maybe (fromMaybe)
 import Data.Set (Set)
 import Data.Set qualified as Set
+import Data.SequenceClass (ViewL((:<), EmptyL), viewl, (|>))
+
+import Data.Sequence.FastQueue (FastQueue)
+import Data.Sequence.FastQueue qualified as FQueue
+
 
 type Coord  = (Int, Int)
 data Cell   = Empty | Wall Time
+type Grid   = Array Coord Cell
 type Dist   = Int
 type Time   = Int
 type Metric = (Down Time, Dist)
 
+
+neighbours :: (Coord, Coord) -> Coord -> [Coord]
+neighbours bounds (x, y) =
+  filter (inRange bounds) [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]
+
+
+-- regular bfs
+bfs :: (Coord, Coord) -> Grid -> Dist
+bfs bounds@(start, end) grid = runST do
+  dists <- newArray bounds maxBound
+  unsafeWrite dists (index bounds start) 0
+  aux dists (FQueue.singleton (0, start))
+  readArray dists end
+  where 
+    aux :: forall s. STUArray s Coord Dist -> FastQueue (Dist, Coord) -> ST s ()
+    aux dists (viewl -> EmptyL) = pure ()
+    aux dists (viewl -> (pDist, p) :< queue) | p == end = pure ()
+    aux dists (viewl -> (pDist, p) :< queue) = do
+      pDist' <- unsafeRead dists (index bounds p)
+      when (pDist' >= pDist) $
+        let
+          processNeighbour :: Coord -> FastQueue (Dist, Coord) -> ST s (FastQueue (Dist, Coord))
+          processNeighbour n queue = do
+            nDist <- unsafeRead dists (index bounds n)
+            let !nDist' = pDist + 1
+            if nDist' >= nDist then pure queue
+            else do
+              unsafeWrite dists (index bounds n) nDist'
+              pure $ queue |> (nDist', n)
+
+          ns :: [Coord]
+          ns = flip filter (neighbours bounds p) \n ->
+                 case unsafeAt grid (index bounds n) of
+                   Empty  -> True
+                   Wall t -> t > 1024
+        in aux dists =<< foldrM processNeighbour queue ns
+
+
 -- poor man's priority queue
-type Queue a = Set a
+type PQueue a = Set a
 pattern EmptyQ   <- (Set.minView -> Nothing    )
-pattern (:<) x q <- (Set.minView -> Just (x, q))
-insert :: Ord a => a -> Queue a -> Queue a
+pattern (:<:) x q <- (Set.minView -> Just (x, q))
+insert :: Ord a => a -> PQueue a -> PQueue a
 insert = Set.insert
 
 -- dijkstra
-findPaths :: (Coord, Coord) -> Bool -> Array Coord Cell -> (Down Time, Dist)
-findPaths bounds@(start, end) part2 grid = runST do
+dijkstra :: (Coord, Coord) -> Grid -> Time
+dijkstra bounds@(start, end) grid = runST do
   dists <- newArray bounds (Down 0, maxBound)
-  writeArray dists start (Down maxBound, 0)
+  unsafeWrite dists (index bounds start) (Down maxBound, 0)
   aux dists (Set.singleton ((Down maxBound, 0), start))
-  readArray dists end
+  getDown . fst <$> unsafeRead dists (index bounds end)
 
   where
-    neighbours :: Coord -> [Coord]
-    neighbours (x, y) =
-      filter (inRange bounds) [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]
-
     aux :: forall s. STArray s Coord (Down Time, Dist)
-        -> Queue (Metric, Coord) -> ST s ()
+        -> PQueue (Metric, Coord) -> ST s ()
     aux dists EmptyQ = pure ()
-    aux dists (((Down pBound, dist), p) :< queue) | p == end = pure ()
-    aux dists (((Down pBound, dist), p) :< queue) = do
-      let
-        -- neighbours and how long they last
-        ns :: [(Coord, Time)]
-        ns = flip mapMaybe (neighbours p) \c -> (c,) <$> 
-          case grid ! c of
-            Empty -> Just pBound
-            Wall t | part2     -> t `min` pBound <$ guard (t > dist)
-                   | otherwise ->         pBound <$ guard (t > 1024)
+    aux dists (((Down !pBound, !dist), p) :<: queue) | p == end = pure ()
+    aux dists ((pD'@(Down !pBound, !dist), p) :<: queue) = do
+      pD <- unsafeRead dists (index bounds p)
+      when (pD' <= pD) do
+        let
+          ns :: [(Coord, Time)]
+          ns = flip mapMaybe (neighbours bounds p) \c -> (c,) <$> 
+            case unsafeAt grid (index bounds c) of
+              Empty  -> Just pBound
+              Wall t -> t `min` pBound <$ guard (t > dist)
 
-        processNeighbour :: (Coord, Time)
-                         -> Queue (Metric, Coord)
-                         -> ST s (Queue (Metric, Coord))
-        processNeighbour (n, nBound') queue = do
-          nD <- readArray dists n
-          let nD' = (Down nBound', dist + 1)
-          if nD' >= nD then pure queue
-          else do
-            writeArray dists n nD'
-            pure $ Set.insert (nD', n) queue
+          processNeighbour :: (Coord, Time)
+                           -> PQueue (Metric, Coord)
+                           -> ST s (PQueue (Metric, Coord))
+          processNeighbour (n, nBound') queue = do
+            nD <- unsafeRead dists (index bounds n)
+            let !nD' = (Down nBound', dist + 1)
+            if nD' >= nD then pure queue
+            else do
+              unsafeWrite dists (index bounds n) nD'
+              pure $ Set.insert (nD', n) queue
 
-      aux dists =<< foldrM processNeighbour queue ns
+        aux dists =<< foldrM processNeighbour queue ns
 
 main :: IO ()
 main = do
   bytes <-
     readFile "inputs/18"
-      <&> strip <&> lines
-      <&> mapMaybe (run $ (,) <$> decimal <* "," <*> decimal)
+      <&> strip
+      <&> run (sepBy ((,) <$> decimal <* "," <*> decimal) "\n")
+      <&> fromMaybe []
 
   let bounds = ((0, 0), (70, 70))
 
   grid :: IOArray Coord Cell <- newArray bounds Empty
-  forM_ (zip [0..] bytes) \(k, b) -> writeArray grid b (Wall k)
+  forM_ (zip bytes (Wall <$> [0..])) $ uncurry $ writeArray grid
   grid <- freeze grid
 
   -- part 1
-  let (_, dist) = findPaths bounds False grid
-  print dist
+  print $ bfs bounds grid
 
   -- part 2
-  let (Down time, _) = findPaths bounds True grid
-  print $ bytes !! time
+  print $ bytes !? dijkstra bounds grid
