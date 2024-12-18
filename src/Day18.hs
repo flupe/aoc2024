f2944577cd009c413ac8bdf663277b951b84e84b
flupe <lucas@escot.me>
2024-12-18 18:45:28 +0100

    cleanup

--- src/Day18.hs
+++ src/Day18.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE NoImplicitPrelude, OverloadedStrings, BlockArguments #-}
+{-# LANGUAGE PatternSynonyms, ViewPatterns #-}
 
 module Day18 (main) where
 
@@ -13,14 +14,20 @@ import Data.Foldable (foldrM)
 import Data.Set (Set)
 import Data.Set qualified as Set
 
+type Coord  = (Int, Int)
+data Cell   = Empty | Wall Time
+type Dist   = Int
+type Time   = Int
+type Metric = (Down Time, Dist)
 
-type Coord = (Int, Int)
-data Cell  = Empty | Wall Time
-type Dist  = Int
-type Time  = Int
-type Queue = Set ((Down Time, Dist), Coord)
+-- poor man's priority queue
+type Queue a = Set a
+pattern EmptyQ   <- (Set.minView -> Nothing    )
+pattern (:<) x q <- (Set.minView -> Just (x, q))
+insert :: Ord a => a -> Queue a -> Queue a
+insert = Set.insert
 
-
+-- dijkstra
 findPaths :: (Coord, Coord) -> Bool -> Array Coord Cell -> (Down Time, Dist)
 findPaths bounds@(start, end) part2 grid = runST do
   dists <- newArray bounds (Down 0, maxBound)
@@ -33,24 +40,30 @@ findPaths bounds@(start, end) part2 grid = runST do
     neighbours (x, y) =
       filter (inRange bounds) [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]
 
-    aux :: forall s. STArray s Coord (Down Time, Dist) -> Queue -> ST s ()
-    aux dists queue | Nothing <- Set.minView queue = pure ()
-    aux dists queue | Just (((Down pBound, dist), p), queue) <- Set.minView queue = do
+    aux :: forall s. STArray s Coord (Down Time, Dist)
+        -> Queue (Metric, Coord) -> ST s ()
+    aux dists EmptyQ = pure ()
+    aux dists (((Down pBound, dist), p) :< queue) | p == end = pure ()
+    aux dists (((Down pBound, dist), p) :< queue) = do
       let
+        -- neighbours and how long they last
+        ns :: [(Coord, Time)]
         ns = flip mapMaybe (neighbours p) \c -> (c,) <$> 
           case grid ! c of
             Empty -> Just pBound
             Wall t | part2     -> t `min` pBound <$ guard (t > dist)
                    | otherwise ->         pBound <$ guard (t > 1024)
 
-        processNeighbour :: (Coord, Time) -> Queue -> ST s Queue
+        processNeighbour :: (Coord, Time)
+                         -> Queue (Metric, Coord)
+                         -> ST s (Queue (Metric, Coord))
         processNeighbour (n, nBound') queue = do
           nD <- readArray dists n
           let nD' = (Down nBound', dist + 1)
           if nD' >= nD then pure queue
           else do
             writeArray dists n nD'
-            pure $ Set.insert (nD', n) $ Set.delete (nD, n) queue
+            pure $ Set.insert (nD', n) queue
 
       aux dists =<< foldrM processNeighbour queue ns
 
